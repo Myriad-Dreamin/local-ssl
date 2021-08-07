d9634adbe0b48b48d068c28d86c85ae0ac4d3a7f
Myriad-Dreamin <camiyoru@gmail.com>
2021-08-07 16:37:19 +0800

    dev: update help command

--- cmd/cmd.help.go
+++ cmd/cmd.help.go
@@ -2,17 +2,27 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"github.com/Myriad-Dreamin/local-ssl/lib/ssl"
+	"os"
+	"path/filepath"
 )
 
 var commandHelpArgs struct {
 	flagSetRef
-	usage func()
+	usage     func()
+	lateUsage func() int
 }
 
 func CommandHelp(*ssl.Env) int {
-	commandHelpArgs.usage()
-	return 0
+	args := &commandHelpArgs
+	if args.flagSet.NArg() == 0 {
+		args.usage()
+		fmt.Printf("Help Usage:\n  %v help [command] - get usage of `command`\n",
+			filepath.Base(os.Args[0]))
+		return 0
+	}
+	return args.lateUsage()
 }
 
 func init() {
@@ -20,4 +30,13 @@ func init() {
 	args := &commandHelpArgs
 	args.flagSet = fs
 	args.usage = usageRoot
+	args.lateUsage = func() int {
+		var cmd = args.flagSet.Arg(0)
+		if command, ok := commands[cmd]; !ok {
+			args.usage()
+		} else {
+			command.set.flagSet.Usage()
+		}
+		return 0
+	}
 }
